2cd62119df756e9cc53ec5f3d8c2456c72a0514e
Awiyanto <awiyanto.ajisasongko@gmail.com>
2023-04-09 22:36:39 +0700

    Add sql script untuk menambah user dan role

---
+++ workfiles/sql/insert-user.sql
@@ -0,0 +1,12 @@
+INSERT INTO T_ROLE (RLID,RLNM) VALUES ('ROLE_ADMIN','Administrator');
+INSERT INTO T_ROLE (RLID,RLNM) VALUES ('ROLE_USER','User');
+
+
+-- Untuk mengencrypt bisa menggunakan https://bcrypt-generator.com
+
+INSERT INTO T_USER (USRNM,USRPWD) VALUES ('admin','$2a$12$ysBlJ/29QZvo0fo0Nj20KeBG1FDYkXp.oxztIZWLPigbpK4c3uvyO');
+INSERT INTO T_USER (USRNM,USRPWD) VALUES ('user','$2a$12$Bd4eEeK0gJsTiXcN42Hjdu3X9UfsjrrTAqf6U0HltwX4JOZ7mnUle');
+
+
+INSERT INTO T_USERROLE (USRNM,RLID) VALUES ('admin','ROLE_ADMIN');
+INSERT INTO T_USERROLE (USRNM,RLID) VALUES ('user','ROLE_USER');
