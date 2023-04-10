20cb4fc19bda85a62977733a1f112ec8d44e8cb4
Rizqi Ahmad Abdillah <rizqiaabdillah@gmail.com>
2023-04-10 23:08:23 +0700

    Update pesertaProgramController

--- src/main/java/com/siska/edu/controller/PesertaProgramController.java
+++ src/main/java/com/siska/edu/controller/PesertaProgramController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +19,10 @@ import com.siska.edu.service.PesertaProgramService;
 @RestController
 @CrossOrigin("*")
 public class PesertaProgramController {
-	
+
 	@Autowired
 	PesertaProgramService pesertaProgramService;
-	
+
 	@RequestMapping("/pesertaprogram")
 	public ResponseEntity<List<PesertaProgram>> getAllPesertaProgram() {
 		return new ResponseEntity<List<PesertaProgram>>(pesertaProgramService.listAll(), HttpStatus.OK);
