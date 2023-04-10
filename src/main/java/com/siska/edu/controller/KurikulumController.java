e88653e5661910346c41a8260ea3003e8e3d3d44
Taopik Romdoni <taopikromdoni@outlook.com>
2023-04-10 09:20:19 +0700

    Menambahkan Fitur CRUD Kurikulum

--- src/main/java/com/siska/edu/controller/KurikulumController.java
+++ src/main/java/com/siska/edu/controller/KurikulumController.java
@@ -5,47 +5,61 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.siska.edu.dao.KurikulumDao;
 import com.siska.edu.model.Kurikulum;
 
-
 @RestController
-@CrossOrigin(origins ="*")
+@CrossOrigin(origins = "*")
+@RequestMapping("/kurikulum")
 public class KurikulumController {
-	
+
 	@Autowired
 	KurikulumDao kd;
-	
-	@GetMapping("/kurikulums")
-	public ResponseEntity<List<Kurikulum>> getCompanyList() {
+
+	@GetMapping
+	public ResponseEntity<List<Kurikulum>> getKurikulumList() {
 		return new ResponseEntity<List<Kurikulum>>((List<Kurikulum>) kd.findAll(), HttpStatus.OK);
 	}
 
-	@PostMapping("/kurikulum")
-	public ResponseEntity<Void> saveOrUpdateCompany(@RequestBody Kurikulum kurikulum) {
+	@PostMapping
+	public ResponseEntity<Void> saveKurikulum(@RequestBody Kurikulum kurikulum) {
+		if (kurikulum.getNama().isBlank() || kurikulum.getNama().isEmpty() || kurikulum.getKeterangan().isBlank()
+				|| kurikulum.getKeterangan().isEmpty()) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
+			kd.save(kurikulum);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	@PutMapping
+	public ResponseEntity<Void> updateKurikulum(@RequestBody Kurikulum kurikulum) {
+		if (kurikulum.getNama().isBlank() || kurikulum.getNama().isEmpty() || kurikulum.getKeterangan().isBlank()
+				|| kurikulum.getKeterangan().isEmpty()) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
+		Integer id = kurikulum.getId();
+		Kurikulum oldKurikulum = kd.findById(id).get();
+		kurikulum.setCreatedBy(oldKurikulum.getCreatedBy());
+		kurikulum.setCreatedAt(oldKurikulum.getCreatedAt());
 		kd.save(kurikulum);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 
-	@DeleteMapping("/kurikulum/{id}")
-	public ResponseEntity<Void> deleteCompany(@PathVariable Integer id) {
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteKurikulum(@PathVariable Integer id) {
 		kd.deleteById(id);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
-	
-//	@GetMapping("/kurikulums/search/{keyword}")
-//	public ResponseEntity<List<Kurikulum>> search(Model m, @PathVariable String keyword) {
-//		return new ResponseEntity<List<Kurikulum>>(kd.search(keyword), HttpStatus.OK);
-//	} 
 
 
 }
---
+++ src/main/resources/static/dist/js/kurikulum.js
@@ -0,0 +1,138 @@
+
+function success() {
+	$("#msg").html("<span style='color: green'>Data kurikulum berhasil ditambahkan !!</span><br>");
+	setTimeout(function() {
+		$('#add').modal('hide');
+		$('#nama').val('');
+		$('#keterangan').val('');
+		$('#msg').html(null);
+	}, 1000);
+	showTable();
+}
+
+function error() {
+	$("#err").html("<span style=\'color:red; font-weight: bold; font-size: 15px;\'>Data tidak berhasil dirubah, silahkan isi data dengan benar</span><br>");
+	setTimeout(function() {
+		$('#err').html(null);
+	}, 3000);
+	showTable();
+}
+function showTable() {
+	$.getJSON('http://localhost:8080/kurikulum', function(json) {
+		$('table').empty();
+		var tr = [];
+		tr.push('<thead>');
+		tr.push('<tr>')
+		tr.push('<th>ID</th>');
+		tr.push('<th>Nama</th>');
+		tr.push('<th>Keterangan</th>');
+		tr.push('<th>Opsi</th>');
+		tr.push('</tr>')
+		tr.push('</thead>');
+		tr.push('<tbody id="kurikulumTable">');
+		for (var i = 0; i < json.length; i++) {
+			tr.push('<tr>');
+			tr.push('<td>' + json[i].id + '</td>');
+			tr.push('<td>' + json[i].nama + '</td>');
+			tr.push('<td>' + json[i].keterangan + '</td>');
+			tr.push('<td ><a class=\'edit\'><button class="btn btn-outline-info btn-sm"><i class="fas fa-edit"></i> Ubah</button></a>&nbsp;&nbsp;<a class=\'delete\' id=' + json[i].id + '><button class="btn btn-outline-danger btn-sm"><i class="fas fa-trash"></i> Hapus</button></a></td>');
+			tr.push('</tr>');
+		}
+		tr.push('</tbody>');
+		$('table').append($(tr.join('')));
+	});
+
+}
+
+
+
+$(document).ready(function() {
+	showTable();
+	$(document).delegate('#addNew', 'click', function(event) {
+		event.preventDefault();
+		var nama = $('#nama').val();
+		var keterangan = $('#keterangan').val();
+		var createdBy = 'taopik';
+
+		$.ajax({
+			type: "POST",
+			contentType: "application/json; charset=utf-8",
+			url: "http://localhost:8080/kurikulum",
+			data: JSON.stringify({ 'nama': nama, 'keterangan': keterangan, 'createdBy': createdBy }),
+			cache: false,
+			success: success,
+			error: function() {
+				$("#msg").html("<span style='color: red'>Pastikan semua data terisi dengan benar.!</span>");
+			}
+		});
+	});
+
+	$("#search").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$("#kurikulumTable tr").filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+
+	$(document).delegate('.delete', 'click', function() {
+		if (confirm('Anda yakin ingin menghapus data ini.?')) {
+			var id = $(this).attr('id');
+			var parent = $(this).parent().parent();
+			$.ajax({
+				type: "DELETE",
+				url: "http://localhost:8080/kurikulum/" + id,
+				cache: false,
+				success: function() {
+					parent.fadeOut('slow', function() {
+						$(this).remove();
+					});
+					showTable()
+				},
+				error: function() {
+					$('#err').html('<span style=\'color:red; font-weight: bold; font-size: 15px;\'>Error deleting record').fadeIn().fadeOut(4000, function() {
+						$(this).remove();
+					});
+				}
+			});
+		}
+	});
+
+	$(document).delegate('.edit', 'click', function() {
+		var parent = $(this).parent().parent();
+
+		var nama = parent.children("td:nth-child(2)");
+		var keterangan = parent.children("td:nth-child(3)");
+		var buttons = parent.children("td:nth-child(4)");
+
+		nama.html("<input required type='text' id='txtName' value='" + nama.html() + "'/>");
+		keterangan.html("<input required type='text' id='txtEmail' value='" + keterangan.html() + "'/>")
+		buttons.html("<button class='btn btn-outline-primary btn-sm' id='save'><i class='fas fa-save'></i> Simpan</button>&nbsp;&nbsp;<a class=\'delete\' id=' + json[i].id + '><button class='btn btn-outline-danger btn-sm'><i class='fas fa-trash'></i>Hapus</button></a>");
+	});
+
+	$(document).delegate('#save', 'click', function() {
+		var parent = $(this).parent().parent();
+		let id = parent.children("td:nth-child(1)");
+		var nama = parent.children("td:nth-child(2)");
+		var keterangan = parent.children("td:nth-child(3)");
+		var buttons = parent.children("td:nth-child(4)");
+		$.ajax({
+			type: "PUT",
+			contentType: "application/json; charset=utf-8",
+			url: "http://localhost:8080/kurikulum",
+			data: JSON.stringify({ 'id': id.html(), 'nama': nama.children("input[type=text]").val(), 'keterangan': keterangan.children("input[type=text]").val(), 'updatedBy': "Romdon" }),
+			cache: false,
+			success: function() {
+				nama.html(nama.children("input[type=text]").val());
+				keterangan.html(keterangan.children("input[type=text]").val());
+				buttons.html("<a class=\'edit\'><button class='btn btn-outline-info btn-sm'><i class='fas fa-edit'></i>Ubah</button></a>&nbsp;&nbsp;<a class=\'delete\' id=' + json[i].id + '><button class='btn btn-outline-danger btn-sm'><i class='fas fa-trash'></i>Hapus</button></a>");
+			},
+			error: error
+		});
+	});
+
+
+
+});
+
+
+
