b724ca2f9f1ef7a0c6639e41dc5b71e22ac3d100
Awiyanto <awiyanto.ajisasongko@gmail.com>
2023-04-12 04:57:21 +0700

    Modify URLs KelasProgram

--- src/main/java/com/siska/edu/controller/KelasProgramController.java
+++ src/main/java/com/siska/edu/controller/KelasProgramController.java
@@ -11,34 +11,36 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.siska.edu.model.KelasProgram;
 import com.siska.edu.service.KelasProgramService;
 
 @RestController
+@RequestMapping("/klpg/api")
 public class KelasProgramController {
     @Autowired
     private KelasProgramService kelasProgramService;
 
-    @GetMapping("/klpg/api/get")
+    @GetMapping("/")
     private ResponseEntity<List<KelasProgram>> FindAll() {
         return new ResponseEntity<List<KelasProgram>>(kelasProgramService.listKelasProgram(), HttpStatus.OK);
     }
 
-    @PostMapping("/klpg/api/post")
+    @PostMapping("/")
     private ResponseEntity<Void> Save(@RequestBody KelasProgram kelasProgram) {
         kelasProgramService.saveKelasProgram(kelasProgram);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
-    @DeleteMapping("/klpg/api/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteKelasProgram(@PathVariable int id) {
         kelasProgramService.deleteKelasProgram(id);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
-    @PatchMapping("/klpg/api/patch")
+    @PatchMapping("/")
     private ResponseEntity<Void> updateKelasProgram(@RequestBody KelasProgram kelasProgram) {
         kelasProgramService.saveKelasProgram(kelasProgram);
         return new ResponseEntity<Void>(HttpStatus.OK);
