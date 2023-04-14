ba36cedb7a88b1177b470faa8ee51d69d4eb4c89
Surya Satria Hidayat <suryasatriahx@gmail.com>
2023-04-14 07:50:01 +0700

    -fix issue : failed update kelas program -adding klpg getbyid method -refractoring script & html klpg

--- src/main/resources/static/dist/js/kelasProgram.js
+++ src/main/resources/static/dist/js/kelasProgram.js
@@ -1,13 +1,8 @@
-const APIs = {
-  getKelasProgram: "http://localhost:8080/klpg/api/",
-  deleteKelasProgram: "http://localhost:8080/klpg/api/",
-  postKelasProgram: "http://localhost:8080/klpg/api/",
-  patchKelasProgram: "http://localhost:8080/klpg/api/",
-};
+const API = "http://localhost:8080/klpg/api/";
 
-$(document).ready(function () {
+function showKelasProgramTable() {
   $("#KelasProgramTable").empty();
-  $.getJSON(APIs.getKelasProgram, function (json) {
+  $.getJSON(API, function (json) {
     var tr = [];
     tr.push("<thead>");
     tr.push("<tr>");
@@ -23,36 +18,47 @@ $(document).ready(function () {
       tr.push("<td>" + json[i].id + "</td>");
       tr.push("<td>" + json[i].kode + "</td>");
       tr.push(
-        "<td><button class='btn btn-primary edit'>Edit</button>&nbsp;&nbsp;<button class='btn btn-primary delete' id=" +
+        "<td><button class='btn btn-primary edit' id=" +
+          json[i].id +
+          ">Edit</button>&nbsp;&nbsp;<button class='btn btn-primary delete' id=" +
           json[i].id +
           ">Delete</button></td>"
       );
       tr.push("</tr>");
     }
-
-    tr.push("</tbody>")
-
+    tr.push("</tbody>");
     $("#KelasProgramTable").append($(tr.join("")));
   });
+}
 
+function searchKelasProgramTable() {
+  $("#KelasProgramSearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#KelasProgramTableBody tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+}
+
+function deleteKelasProgram() {
   $(document).delegate(".delete", "click", function () {
     if (confirm("Hapus kelas program ini?")) {
       var id = $(this).attr("id");
       var parent = $(this).parent().parent();
       $.ajax({
         type: "DELETE",
-        url: APIs.deleteKelasProgram + id,
+        url: API + id,
         cache: false,
         success: function () {
           parent.fadeOut("slow", function () {
             $(this).reve();
           });
-          location.reload(true);
+          showKelasProgramTable();
         },
         error: function () {
           $("#err")
             .html(
-              "<span style='color:red; font-weight: bold; font-size: 30px;'>Error deleting record"
+              "<span style='color:red; font-weight: bold; font-size: 30px;'>Gagal menghapus kode</span>"
             )
             .fadeIn()
             .fadeOut(4000, function () {
@@ -62,48 +68,86 @@ $(document).ready(function () {
       });
     }
   });
+}
 
-  $(document).delegate("#save", "click", function () {
-    var parent = $(this).parent().parent();
-
-    var id = parent.children("td:nth-child(1)");
-    var kode = parent.children("td:nth-child(2)");
-
-    $.ajax({
-      type: "POST",
-      contentType: "application/json; charset=utf-8",
-      url: APIs.postKelasProgram,
-      data: JSON.stringify({
-        id: id.html(),
-        kode: kode.children("input[type=text]").val(),
-      }),
-      cache: false,
-      success: function () {
-        kode.html(kode.children("input[type=text]").val());
-        buttons.html(
-          "<button class='edit' id='" +
-            id.html() +
-            "'>Edit</button>&nbsp;&nbsp;<button class='delete' id='" +
-            id.html() +
-            "'>Delete</button>"
-        );
+function addKelasProgram() {
+  $("#addkelasprogramform").validate({
+    rules: {
+      kelasprogram: {
+        required: true,
+        rangelength: [1, 5],
       },
-      error: function () {
-        $("#err")
-          .html(
-            "<span style='color:red; font-weight: bold; font-size: 30px;'>Error updating record"
-          )
-          .fadeIn()
-          .fadeOut(4000, function () {
-            $(this).remove();
-          });
+    },
+    messages: {
+      kelasprogram: {
+        required: "Input tidak boleh kosong",
+        rangelength: function (range, input) {
+          return [
+            "Anda memasukkan ",
+            $(input).val().length,
+            " karakter. Maksimum ",
+            range[1],
+            " karakter.",
+          ].join("");
+        },
       },
-    });
+    },
+    submitHandler: function () {
+      var kodeKelasProgram = $("#kelasprogram").val();
+      $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: API,
+        data: JSON.stringify({
+          kode: kodeKelasProgram,
+          createdBy: $("#username-login").html(),
+        }),
+        cache: false,
+
+        success: function () {
+          $("#msg").html(
+            "<span style='color: green'>Berhasil menambahkan kelas program baru.</span>"
+          );
+          showKelasProgramTable();
+          window.setTimeout(function () {
+            $("#tambahkelasprogrammodal").modal("hide");
+          }, 1000);
+        },
+        error: function () {
+          $("#err")
+            .html(
+              "<span style='color:red; font-weight: bold; font-size: 30px;'>Error inserting record"
+            )
+            .fadeIn()
+            .fadeOut(4000, function () {
+              $(this).remove();
+            });
+        },
+      });
+    },
   });
+}
+
+$(document).ready(function () {
+  // Menipulasi tampilan menu
+  $("li.nav-item").removeClass("menu-open"); // remove class menu-open pada semua li yang aktif
+  $("#menu_nilai_2").addClass("active"); // tambahkan class active pada a dengan id menu-mapel-1
+  $("#menu_nilai").addClass("active").parent().addClass("menu-open"); // tambahkan class active pada a dengan id menu-mapel-1 lalu pada parentnya (li) ditambahkan class menu-open
+
+  showKelasProgramTable();
+  searchKelasProgramTable();
+  deleteKelasProgram();
+  addKelasProgram();
 
   $(document).delegate(".edit", "click", function () {
+    var id = $(this).attr("id");
     var parent = $(this).parent().parent();
-    var id = parent.children("td:nth-child(1)");
+
+    $.getJSON(API + id, function (json) {
+      $("#creator-name").text(json.createdBy);
+    });
+
+    id = parent.children("td:nth-child(1)");
     var kode = parent.children("td:nth-child(2)");
     var buttons = parent.children("td:nth-child(3)");
 
@@ -115,70 +159,21 @@ $(document).ready(function () {
     );
   });
 
-  $(document).delegate("#TambahKelasProgram", "click", function (event) {
-    event.preventDefault();
-
-    var kodeKelasProgram = $("#kelasprogram").val();
-
-    $.ajax({
-      type: "POST",
-      contentType: "application/json; charset=utf-8",
-      url: APIs.postKelasProgram,
-      data: JSON.stringify({
-        kode: kodeKelasProgram,
-        createdBy: "surya", //ini nanti ngambil usernya gimana
-      }),
-      cache: false,
-
-      success: function (result) {
-        $("#msg").html(
-          "<span style='color: green'>Berhasil menambahkan kelas program baru.</span>"
-        );
-        window.setTimeout(function () {
-          location.reload();
-        }, 1000);
-      },
-      error: function (err) {
-        $("#msg").html(
-          "<span style='color: red'>Gagal menambahkan kelas program baru.</span>"
-        );
-      },
-    });
-  });
-
-  $(document).delegate("#cancel", "click", function () {
+  $(document).delegate("#update", "click", function () {
     var parent = $(this).parent().parent();
-
     var id = parent.children("td:nth-child(1)");
     var kode = parent.children("td:nth-child(2)");
     var buttons = parent.children("td:nth-child(3)");
 
-    $(document).delegate("#update", "click", function () {
-      var parent = $(this).parent().parent();
-
-      var id = parent.children("td:nth-child(1)");
-      var kode = parent.children("td:nth-child(2)");
-      var buttons = parent.children("td:nth-child(3)");
-
-      kode.html(kode.children("input[type=text]").val());
-      buttons.html(
-        "<button class='btn btn-primary edit' id='" +
-          id.html() +
-          "'>Edit</button>&nbsp;&nbsp;<button class='btn btn-primary delete' id='" +
-          id.html() +
-          "'>Delete</button>"
-      );
-    });
-
     $.ajax({
       type: "PATCH",
       contentType: "application/json; charset=utf-8",
-      url: APIs.patchKelasProgram,
+      url: API,
       data: JSON.stringify({
         id: id.html(),
         kode: kode.children("input[type=text]").val(),
-        createdBy: "surya", //cari tau cara ngambil createdby nya
-        updatedBy: "suryalagi", //ini ngambil user nanti
+        createdBy: $("#creator-name").text(),
+        updatedBy: $("#username-login").html(),
       }),
       cache: false,
 
@@ -205,11 +200,7 @@ $(document).ready(function () {
     });
   });
 
-  $("#KelasProgramSearch").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#KelasProgramTableBody tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+  $(document).delegate("#cancel", "click", function () {
+    showKelasProgramTable();
   });
-
 });
